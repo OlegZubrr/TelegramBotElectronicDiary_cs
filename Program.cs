32ccd644764fe98b94f71653a9e0e6abd7d38781
Oleg Zubr <olegzubr769@gmail.com>
2025-01-17 16:30:59 +0300

    Initialization

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TelegramBotEFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
